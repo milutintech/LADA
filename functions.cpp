f37cf1789dc88a76bf8e4c1fad25422b45a54416
Obili69 <christian.magnus@obrecht.ch>
2023-11-08 17:33:51 +0100

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,17 @@
 
+//Function to calculate the torque request for the DMC with 5 samples
+
+int16_t calculateTorque5S(int16_t sensorSim[5],bool reverseSig){
+  int16_t SampeldPotiValue = 0;
+  int16_t DMC_TorqueCalc = 0;
+
+  for(int i = 0; i < 5; i++){
+    SampeldPotiValue = SampeldPotiValue + sensorSim[i];
+  }
+  SampeldPotiValue = SampeldPotiValue / 5;
+  DMC_TorqueCalc = map(SampeldPotiValue, 0, 4096, 0, 32767);
+  if(reverseSig){
+    DMC_TorqueCalc = 0 - DMC_TorqueCalc;
+  }
+  return DMC_TorqueCalc;
+}
