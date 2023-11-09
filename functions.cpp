218c0eefec0064553dc9fe6cf904cf923447f37b
Obili69 <102026699+Obili69@users.noreply.github.com>
2023-11-09 08:07:36 +0100

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,12 @@
 
 //Function to calculate the torque request for the DMC with 5 samples
 
-int16_t calculateTorque5S(int16_t sensorSim[5],bool reverseSig){
+int16_t calculateTorque5S(int16_t sampleSetPedal[5],bool reverseSig){
   int16_t SampeldPotiValue = 0;
   int16_t DMC_TorqueCalc = 0;
 
   for(int i = 0; i < 5; i++){
-    SampeldPotiValue = SampeldPotiValue + sensorSim[i];
+    SampeldPotiValue = SampeldPotiValue + sampleSetPedal[i];
   }
   SampeldPotiValue = SampeldPotiValue / 5;
   DMC_TorqueCalc = map(SampeldPotiValue, 0, 4096, 0, 32767);
