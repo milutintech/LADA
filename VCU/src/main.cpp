605a2ed78e5b20b342d61c7db857634ede866e51
Obili69 <christian.magnus@obrecht.ch>
2023-11-16 09:09:26 +0100

    Update main.cpp

--- VCU/src/main.cpp
+++ VCU/src/main.cpp
@@ -52,12 +52,21 @@ mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 #define MAX_DATA_SIZE 8
 
 //Defining CAN Indexes
+#define CAN_2515
+
+
 #define BSC_COMM 0x260
 #define BSC_LIM 0x261
-#define CAN_2515
+
 #define DMCCTRL 0x210
 #define DMCLIM 0x211
 #define DMCCTRL2 0x212
+
+#define NLG_DEM_LIM 0x711
+#define NLG_ACT_ERR 0x799
+#define NLG_ACT_LIM 0x728
+#define NLG_ACT_PLUG 0x739
+
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
@@ -161,6 +170,76 @@ uint8_t sampleSetCounter = 0;
 int16_t sampleSetPedal[5] = {0,0,0,0,0};
 bool reversSig = 0;
 uint8_t VehicleMode = Run;
+
+//*********************************************************************//
+//Deffining Variables for Can transmission
+//NLG
+//*********************************************************************//
+
+//Sending Variables
+//Variables for 0x711
+bool NLG_C_ClrError = 0;
+bool NLG_C_UnlockConRq = 0;
+bool NLG_C_VentiRq = 0;
+int  NLG_DcHvVoltLimMax = 400;
+int NLG_DcHvVoltLimMax_Scale = 0;
+int NLG_DcHvCurrLimMax = 50;
+int NLG_DcHvCurrLimMax_Scale = 0;
+uint16_t NLG_AcCurrLimMax_Scale = 0;
+uint8_t NLG_StateDem = 0;
+uint16_t NLG_LedDem = 0;
+uint16_t NLG_AcCurrLimMax = 0;
+
+bool NLG_C_EnPhaseShift = 0;
+uint16_t NLG_AcPhaseShift = 0;
+uint16_t NLG_AcPhaseShift_Scale = 0;
+uint16_t prep = 0;
+
+//recive Variable
+//Variables for 0x709
+uint16_t NLG_AcWhAct = 0;
+uint16_t NLG_DcHvWhAct = 0;
+uint16_t NLG_MaxTempAct = 0;
+int16_t NLG_TempCon = 0;
+float NLG_DcHvAhAct = 0;
+//Variables for 0x728
+uint8_t NLG_StateCtrlPilot = 0;
+uint16_t NLG_DcHvVoltAct = 0;
+uint8_t NLG_StateAct = 0;
+bool NLG_S_DcHvCurrLim = 0;
+bool NLG_S_DcHvVoltLim = 0;
+int16_t NLG_AcPhaseUsdNLG_S_DcHvVoltLim = 0;
+bool NLG_S_ProximityLim = 0;
+bool NLG_S_CtrlPilotLim = 0;
+bool NLG_S_ConTempLim = 0;
+bool NLG_S_IntTempLim = 0;
+bool NLG_S_AcCurrLim = 0;
+int16_t NLG_AcCurrMaxAct = 0;
+uint16_t NLG_AcCurrHwAvl = 0;
+bool NLG_S_ProximityDet = 0;
+bool NLG_S_CtrlPilotDet = 0;
+bool NLG_S_ConLocked = 0;
+bool NLG_S_AcDet = 0;
+bool NLG_S_HwWakeup = 0;
+bool NLG_S_HwEnable = 0;
+bool NLG_S_Err = 0;
+bool NLG_S_War = 0;
+uint16_t NLG_DcHvCurrAct = 0;
+//Variables for 0x739
+uint8_t NLG_ACT_PLUGCom = 0;
+uint8_t NLG_StatusCP = 0;
+bool NLG_S_CP_X1 = 0;
+bool NLG_S_CP_SCC = 0;
+uint16_t NLG_AcCurrMaxCP = 0;
+uint8_t NLG_StatusPP = 0;
+uint8_t NLG_AcCurrMaxPP = 0;
+bool NLG_S_AcVoltDerating = 0;
+bool NLG_S_AcDeratingNoisy = 0;
+uint8_t NLG_AcPhaseUsd = 0;
+uint8_t NLG_AcPhaseDet = 0;
+uint8_t NLG_CoolingRequest = 0;
+int16_t NLG_TempCoolPlate = 0;
+
 //*********************************************************************//
 //Deffining Variables for Can transmission
 //DMC
@@ -264,7 +343,8 @@ uint32_t id;
 uint8_t  type; // bit0: ext, bit1: rtr
 uint8_t  len;
 
-byte readDataBSC[MAX_DATA_SIZE] = {0}; //Storage for recived data
+byte readDataBSC[MAX_DATA_SIZE] = {0}; //Storage for recived data BSC
+byte readDataNLG[MAX_DATA_SIZE] = {0}; //Storage for recived data NLg
 unsigned char controllBufferDMC[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char controllBuffer2DMC[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char limitBufferDMC[8] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -442,6 +522,7 @@ void sendBSC(){
   CAN.sendMsgBuf(BSC_LIM, 0, 6, limitBufferBSC);
 }
 void sendDMC(){
+
   DMC_TrqSlewrate_Scale = DMC_TrqSlewrate * 50;
   DMC_MechPwrMaxMot_Scale = DMC_MechPwrMaxMot / 4;
   DMC_MechPwrMaxGen_Scale = DMC_MechPwrMaxGen / 4;
@@ -539,8 +620,90 @@ void reciveDMC(){
     }
   }
 }
+void sendNLG(){
+    NLG_DcHvVoltLimMax_Scale = NLG_DcHvVoltLimMax * 10;
+    NLG_DcHvCurrLimMax_Scale = NLG_DcHvCurrLimMax * 10;
+    NLG_DcHvCurrLimMax_Scale -= 1024;
+    NLG_AcCurrLimMax_Scale = NLG_AcCurrLimMax * 10;
+    NLG_AcCurrLimMax_Scale -= 1024;
+    NLG_AcPhaseShift_Scale = NLG_AcPhaseShift * 10;
+    controllBufferNLG1[0] = NLG_C_ClrError << 8 | NLG_C_UnlockConRq << 7 | NLG_C_VentiRq << 6 | (NLG_DcHvVoltLimMax_Scale >> 8) & 0x1F;
+    controllBufferNLG1[1] = NLG_DcHvVoltLimMax_Scale & 0x00FF;
+    controllBufferNLG1[2] = (NLG_StateDem << 5)  | (NLG_DcHvCurrLimMax_Scale >> 8)& 0x07;
+    controllBufferNLG1[3] = NLG_DcHvCurrLimMax_Scale & 0x00FF;
+    controllBufferNLG1[4] = NLG_LedDem << 4 | (NLG_AcCurrLimMax_Scale << 8) & 0x07;
+    controllBufferNLG1[5] = NLG_AcCurrLimMax_Scale & 0x00FF;
+    controllBufferNLG1[6] = NLG_C_EnPhaseShift << 4 | (NLG_AcPhaseShift_Scale >> 8) & 0x07;
+    controllBufferNLG1[7] = NLG_AcPhaseShift_Scale & 0x00FF;
+    CAN.sendMsgBuf(NLG_DEM_LIM, 0, 8, controllBufferNLG1);
+    delay(100);                       // send data per 100ms
+   
+}
+void reciveNLG(){
+
+    if (CAN_MSGAVAIL != CAN.checkReceive()) {
+        return;
+    }
 
 
+
+   
+    // read data, len: data length, buf: data buf
+    CAN.readMsgBuf(&len, readDataNLG);
+
+    id = CAN.getCanId();
+    type = (CAN.isExtendedFrame() << 0) |
+           (CAN.isRemoteRequest() << 1);
+    
+    
+    
+    switch (id){
+      case NLG_ACT_LIM:
+        NLG_StateCtrlPilot = readDataNLG[0] >> 5;
+        NLG_DcHvVoltAct = (readDataNLG[0] << 8) & 0x1F00;
+        NLG_DcHvVoltAct = NLG_DcHvVoltAct | readDataNLG[1];
+        NLG_StateAct = readDataNLG[2] >> 5;
+        NLG_S_DcHvCurrLim = readDataNLG[2] >> 4 & 0x01;
+        NLG_S_DcHvVoltLim = readDataNLG[2] >> 3 & 0x01;
+        NLG_DcHvCurrAct = (readDataNLG[2] << 8) & 0x07;
+        NLG_DcHvCurrAct = NLG_DcHvCurrAct | readDataNLG[3];
+        NLG_S_ProximityLim = readDataNLG[4] >> 7;
+        NLG_S_CtrlPilotLim = (readDataNLG[4] >> 6) & 0x01;
+        NLG_S_ConTempLim = (readDataNLG[4] >> 5) & 0x01;
+        NLG_S_IntTempLim = (readDataNLG[4] >> 4) & 0x01;
+        NLG_S_AcCurrLim= (readDataNLG[4] >> 5) & 0x01;
+        NLG_AcCurrMaxAct = (readDataNLG[4] << 8) & 0x07;
+        NLG_AcCurrMaxAct = NLG_AcCurrMaxAct | readDataNLG[5];
+        NLG_AcCurrHwAvl = readDataNLG[6];
+        NLG_S_ProximityDet = readDataNLG[7] >> 7;
+        NLG_S_CtrlPilotDet = (readDataNLG[7] >> 6) & 0x01;
+        NLG_S_ConLocked = (readDataNLG[7] >> 5) & 0x01;
+        NLG_S_AcDet = (readDataNLG[7] >> 4) & 0x01;
+        NLG_S_HwWakeup = (readDataNLG[7] >> 3) & 0x01;
+        NLG_S_HwEnable = (readDataNLG[7] >> 2) & 0x01;
+        NLG_S_Err = (readDataNLG[7] >> 1) & 0x01;
+        NLG_S_War = readDataNLG[7]& 0x01;
+      break;
+      case NLG_ACT_PLUG:
+        NLG_StatusCP = (readDataNLG[0] >> 5) & 0x07;
+        NLG_S_CP_X1 = (readDataNLG[0] >> 4) & 0x01;
+        NLG_S_CP_SCC = (readDataNLG[0] >> 3) & 0x01;
+        NLG_AcCurrMaxCP = (readDataNLG[0] << 8) & 0x03;
+        NLG_AcCurrMaxCP = NLG_AcCurrMaxCP | readDataNLG[1];
+        NLG_StatusPP = (readDataNLG[2] >> 4) & 0x07;
+        NLG_AcCurrMaxPP = readDataNLG[2] & 0x0F;
+        NLG_S_AcVoltDerating = readDataNLG[3] >> 7;
+        NLG_S_AcDeratingNoisy = (readDataNLG[3] >> 6) & 0x01;
+        NLG_AcPhaseUsd = (readDataNLG[3] >> 5) & 0x01;
+        NLG_AcPhaseDet = (readDataNLG[3] >> 2) & 0x01;
+        NLG_CoolingRequest = (readDataNLG[4] >> 6) & 0x01;
+        NLG_ACT_PLUGCom = readDataNLG[4];
+      break;
+      case NLG_ACT_ERR:
+
+      break;
+    }
+}
 //*********************************************************************//
 //Functions for ADAC
 //Inputs
